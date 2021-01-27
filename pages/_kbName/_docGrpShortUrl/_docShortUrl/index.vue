2b7d47ae7c74f95adb3ab761acb29f5ee9887ece
h31 <h31@h31deiMac.local>
2021-01-27 15:46:08 +0800

    增加layout

---
+++ pages/_kbName/_docGrpShortUrl/_docShortUrl/index.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>33333
+    {{kbInfo.docShortUrl}}
+    {{kbInfo.docShortUrl}}
+    {{kbInfo.kbName}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "docShortUrl",
+  layout: 'kbName',
+  data() {
+    return {
+
+      kbInfo: {kbName: "", docGrpShortUrl: "", docShortUrl: "", grpName: "", docName: ""},
+      docInfo: [],
+
+    };
+  },
+  created() {
+    this.kbInfo.docShortUrl = this.$route.params.docShortUrl
+    this.kbInfo.docGrpShortUrl = this.$route.params.docGrpShortUrl
+    this.kbInfo.kbName = this.$route.params.kbName
+  },
+  mounted() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+.is-current > .el-tree-node__content {
+  color: orange !important;
+}
+
+</style>
